cfcaf9cc14c423b56428213fd69fb99293dcc7c2
Leo Chen <leo.chen.shc@gmail.com>
2022-11-21 16:51:43 -0800

    files tested and ready to begin the server build

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: 'mysql',
+  client: "mysql",
   connection: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     database: process.env.DB_LOCAL_DBNAME,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
