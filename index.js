acc283166cfcae7e965c83bc72c89d25f17ba744
Leo Chen <leo.chen.shc@gmail.com>
2022-11-21 17:37:39 -0800

    merging the changes of the index.js file, commented out Travis' codes for now and will discuss it later

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 8080;
 
 const warehouseRoutes = require("./routes/warehouseRoute");
 
-// home route:  we can safely delete this
-// app.get("/", (req, res) => {
-//   res.send("Welcome to my API");
-// });
+// home page
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to Terrace's API for inStock. By Mahdi, Travis, Gurpreeet, and Leo."
+  );
+});
 
 // all warehouses routes
 app.use("/warehouses", warehouseRoutes);
@@ -16,25 +18,25 @@ app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
 });
 // setup dotenv to support ports by run environment
-const path = require("node:path");
-require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+// const path = require("node:path");
+// require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
-// initialize express server
-const express = require("express");
-const app = express();
+// // initialize express server
+// const express = require("express");
+// const app = express();
 
-const warehouseRouter = require("./routes/warehouses");
-const inventoryRouter = require("./routes/inventory");
+// const warehouseRouter = require("./routes/warehouses");
+// const inventoryRouter = require("./routes/inventory");
 
-// setup middleware: CORS for client requests, express.json to handle data
-app.use(cors());
-app.use(express.json());
+// // setup middleware: CORS for client requests, express.json to handle data
+// app.use(cors());
+// app.use(express.json());
 
-// routes for warehouse resource
-app.use("/warehouses", warehouseRouter);
-app.use("/inventory", inventoryRouter);
+// // routes for warehouse resource
+// app.use("/warehouses", warehouseRouter);
+// app.use("/inventory", inventoryRouter);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`server running: ${PORT}`);
-});
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//   console.log(`server running: ${PORT}`);
+// });
